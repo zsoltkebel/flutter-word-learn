6f87360fa4c8e315439e780e8610ea08584387c4
zsoltkebel <kebel.zsolt@gmail.com>
2022-07-10 17:44:26 +0200

    Improve interface of sharing a collection

--- lib/screens/user_selection.dart
+++ lib/screens/user_selection.dart
@@ -2,7 +2,7 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:word_learn/model/friend_model.dart';
 
-typedef OnUserSelected = void Function(Set<UserInfoModel> uids);
+typedef OnUserSelected = void Function(List<UserInfoModel> uids);
 
 class UserSelectionScreen extends StatefulWidget {
   final Set<String>? uids; // uid-s of the initially selected users
@@ -19,9 +19,11 @@ class UserSelectionScreen extends StatefulWidget {
 }
 
 class _UserSelectionScreenState extends State<UserSelectionScreen> {
+  final textEditingController = TextEditingController();
+
   List<UserInfoModel> users = [];
   List<UserInfoModel> matches = [];
-  Set<UserInfoModel> selected = {};
+  List<UserInfoModel> selected = [];
 
   @override
   initState() {
@@ -30,8 +32,14 @@ class _UserSelectionScreenState extends State<UserSelectionScreen> {
       users = snapshot.docs.map(UserInfoModel.fromSnapshot).toList();
       selected = users
           .where((user) => widget.uids?.contains(user.uid) ?? false)
-          .toSet();
+          .toList();
     });
+  }
+
+  @override
+  dispose() {
+    textEditingController.dispose();
+    super.dispose();
   }
 
   @override
@@ -39,6 +47,11 @@ class _UserSelectionScreenState extends State<UserSelectionScreen> {
     return Scaffold(
       appBar: AppBar(
         title: TextField(
+          controller: textEditingController,
+          decoration: const InputDecoration(
+            hintText: 'Search',
+            border: InputBorder.none,
+          ),
           onChanged: _onFilterTextChanged,
         ),
       ),
@@ -54,38 +67,7 @@ class _UserSelectionScreenState extends State<UserSelectionScreen> {
             users =
                 snapshot.data!.docs.map(UserInfoModel.fromSnapshot).toList();
           }
-          return ListView.builder(
-            itemCount: matches.length,
-            itemBuilder: (context, index) {
-              return ListTile(
-                onTap: () {
-                  setState(() {
-                    if (!isSelected(index)) {
-                      selected.add(matches[index]);
-                    } else {
-                      selected.removeWhere(
-                          (user) => user.uid == matches[index].uid);
-                    }
-                    print(selected);
-                  });
-                },
-                leading: Text(matches[index].displayName!),
-                trailing: Checkbox(
-                  value: isSelected(index),
-                  onChanged: (value) {
-                    setState(() {
-                      if (value!) {
-                        selected.add(matches[index]);
-                      } else {
-                        selected.removeWhere(
-                            (user) => user.uid == matches[index].uid);
-                      }
-                    });
-                  },
-                ),
-              );
-            },
-          );
+          return _buildBackground();
         },
       ),
       floatingActionButton: FloatingActionButton(
@@ -93,7 +75,7 @@ class _UserSelectionScreenState extends State<UserSelectionScreen> {
           Navigator.pop(context);
           widget.onSelected.call(selected);
         },
-        child: Text('Done'),
+        child: const Text('Done'),
       ),
     );
   }
@@ -111,5 +93,79 @@ class _UserSelectionScreenState extends State<UserSelectionScreen> {
       }
       print(selected);
     });
+  }
+
+  Widget _buildBackground() {
+    if (textEditingController.text.isEmpty) {
+      if (selected.isEmpty) {
+        return const Center(
+          child: const Text('Search for a user.'),
+        );
+      } else {
+        return ListView.builder(
+          itemCount: selected.length,
+          itemBuilder: (context, index) {
+            return ListTile(
+              onTap: () {
+                setState(() {
+                  selected
+                      .removeWhere((user) => user.uid == selected[index].uid);
+                });
+              },
+              leading: Text(selected[index].displayName!),
+              trailing: Checkbox(
+                value: true,
+                onChanged: (value) {
+                  setState(() {
+                    if (value!) {
+                      selected.add(selected[index]);
+                    } else {
+                      selected.removeWhere(
+                          (user) => user.uid == selected[index].uid);
+                    }
+                  });
+                },
+              ),
+            );
+          },
+        );
+      }
+    } else if (matches.isEmpty) {
+      return const Center(
+        child: Text('No user found.'),
+      );
+    } else {
+      return ListView.builder(
+        itemCount: matches.length,
+        itemBuilder: (context, index) {
+          return ListTile(
+            onTap: () {
+              setState(() {
+                if (!isSelected(index)) {
+                  selected.add(matches[index]);
+                } else {
+                  selected
+                      .removeWhere((user) => user.uid == matches[index].uid);
+                }
+              });
+            },
+            leading: Text(matches[index].displayName!),
+            trailing: Checkbox(
+              value: isSelected(index),
+              onChanged: (value) {
+                setState(() {
+                  if (value!) {
+                    selected.add(matches[index]);
+                  } else {
+                    selected
+                        .removeWhere((user) => user.uid == matches[index].uid);
+                  }
+                });
+              },
+            ),
+          );
+        },
+      );
+    }
   }
 }
