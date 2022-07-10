e84d3336f3d68b996ebd34fc3897d577938d7295
zsoltkebel <kebel.zsolt@gmail.com>
2022-07-10 11:36:30 +0200

    Refactor home screen

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,9 +1,16 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-import 'package:word_learn/view/folders_page.dart';
+import 'package:word_learn/model/user_data.dart';
+import 'package:word_learn/screens/collections.dart';
+import 'package:word_learn/screens/friends.dart';
 
 class HomeScreen extends StatefulWidget {
-  const HomeScreen({Key? key}) : super(key: key);
+  final UserData? userData;
+
+  const HomeScreen({
+    Key? key,
+    this.userData,
+  }) : super(key: key);
 
   @override
   State<HomeScreen> createState() => _HomeScreenState();
@@ -12,14 +19,7 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   int _selectedIndex = 0;
 
-  static final List<Widget> _screens = <Widget>[
-    const FoldersPage(),
-    Center(
-      child: ElevatedButton(onPressed: () async {
-        await FirebaseAuth.instance.signOut();
-      }, child: Text('Sign Out')),
-    ),
-  ];
+  List<Widget> _screens = [];
 
   void _onItemTapped(int index) {
     setState(() {
@@ -28,21 +28,76 @@ class _HomeScreenState extends State<HomeScreen> {
   }
 
   @override
+  void initState() {
+    super.initState();
+    _screens = <Widget>[
+      const CollectionsPage(),
+      const FriendsPage(),
+      Center(
+        child: ElevatedButton(
+            onPressed: () async {
+              await FirebaseAuth.instance.signOut();
+            },
+            child: const Text('Sign Out')),
+      ),
+    ];
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: _screens.elementAt(_selectedIndex),
+      // body: _screens.elementAt(_selectedIndex),
+      body: Stack(
+        children: [
+          Offstage(
+            offstage: _selectedIndex != 0,
+            child: _screens[0],
+          ),
+          Offstage(
+            offstage: _selectedIndex != 1,
+            child: _screens[1],
+          ),
+          Offstage(
+            offstage: _selectedIndex != 2,
+            child: _screens[2],
+          ),
+          Positioned(
+            bottom: 0.0,
+            child: IgnorePointer(
+              child: Container(
+                height: 40.0,
+                width: MediaQuery.of(context).size.width,
+                decoration: BoxDecoration(
+                  gradient: LinearGradient(
+                    begin: Alignment.topCenter,
+                    end: Alignment.bottomCenter,
+                    colors: [
+                      Theme.of(context).scaffoldBackgroundColor.withOpacity(0),
+                      Theme.of(context).scaffoldBackgroundColor,
+                    ],
+                  ),
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
       bottomNavigationBar: BottomNavigationBar(
-        showSelectedLabels: false,
-        showUnselectedLabels: false,
-        items: <BottomNavigationBarItem>[
+        elevation: 0.0,
+        items: const <BottomNavigationBarItem>[
           BottomNavigationBarItem(
             icon: Icon(Icons.folder_copy_outlined),
-            label: 'Home',
-            activeIcon: Icon(Icons.folder_copy)
+            activeIcon: Icon(Icons.folder_copy),
+            label: 'Collections',
           ),
           BottomNavigationBarItem(
-            icon: Icon(_selectedIndex == 1 ? Icons.people : Icons.people_outlined),
-            label: 'Business',
+            icon: Icon(Icons.people_outlined),
+            activeIcon: Icon(Icons.people),
+            label: 'Friends',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.logout),
+            label: 'Logout',
           ),
         ],
         currentIndex: _selectedIndex,
