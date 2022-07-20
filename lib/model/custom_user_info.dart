71cf7fb9b0bee878a9fe13b87b98b98f6b608548
zsoltkebel <kebel.zsolt@gmail.com>
2022-07-20 23:10:16 +0200

    Rename class

--- lib/model/custom_user_info.dart
+++ lib/model/custom_user_info.dart
@@ -1,19 +1,19 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 
-class UserInfoModel {
+class CustomUserInfo {
   final String uid;
   final String? displayName;
   final String? photoURL;
   final List<String?> friendUIDs;
 
-  UserInfoModel.fromSnapshot(DocumentSnapshot doc)
+  CustomUserInfo.fromSnapshot(DocumentSnapshot doc)
       : uid = doc.id,
         displayName = doc['display-name'],
         photoURL = doc['photo-url'],
         friendUIDs = List.from(doc['friend-uid-array']);
 
-  UserInfoModel.fromUser(User user)
+  CustomUserInfo.fromUser(User user)
       : uid = user.uid,
         displayName = user.displayName,
         photoURL = user.photoURL,
